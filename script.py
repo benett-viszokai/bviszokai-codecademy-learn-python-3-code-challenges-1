48c50f6217e03e578ba01b9d9ea2d907cae6bdc3
benett-viszokai <benettviszokai@gmail.com>
2024-07-29 19:33:10 +0200

    Update script.py

--- script.py
+++ script.py
@@ -280,3 +280,88 @@ def combine_sort(my_list1, my_list2):
 
 #Uncomment the line below when your function is done
 print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+########################################################################################################################################################
+
+# IV. Lists (Advanced)
+
+# 1. Every Three Numbers
+
+# Create a function called every_three_nums that has one parameter named start.
+# The function should return a list of every third number between start and 100 (inclusive). For example, every_three_nums(91) should return the list [91, 94, 97, 100]. If start is greater than 100, the function should return an empty list.
+
+#Write your function here
+def every_three_nums(start):
+  if start > 100:
+    return []
+  else:
+    return list(range(start, 101, 3))
+
+#Uncomment the line below when your function is done
+print(every_three_nums(2))
+
+# 2. Remove Middle
+
+# Create a function named remove_middle which has three parameters named my_list, start, and end.
+# The function should return a list where all elements in my_list with an index between start and end (inclusive) have been removed.
+# For example, the following code should return [4, 23, 42] because elements at indices 1, 2, and 3 have been removed:
+
+#Write your function here
+def remove_middle(my_list, start, end):
+  first_half = my_list[0:start]
+  second_half = my_list[end + 1:]
+  removed = first_half + second_half
+  return removed
+
+#Uncomment the line below when your function is done
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+
+# 3. More Frequent Item
+
+# Create a function named more_frequent_item that has three parameters named my_list, item1, and item2.
+# Return either item1 or item2 depending on which item appears more often in my_list.
+# If the two items appear the same number of times, return item1.
+
+#Write your function here
+def more_frequent_item(my_list, item1, item2):
+  if my_list.count(item2) > my_list.count(item1):
+    return item2
+  else:
+    return item1
+#Uncomment the line below when your function is done
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2], 2, 3))
+
+# 4. Double Index
+
+# Create a function named double_index that has two parameters: a list named my_list and a single number named index.
+# The function should return a new list where all elements are the same as in my_list except for the element at index. The element at index should be double the value of the element at index of the original my_list.
+# If index is not a valid index, the function should return the original list.
+
+#Write your function here
+def double_index(my_list, index):
+  if index > len(my_list) - 1:
+    return my_list
+  else:
+    changed_index = my_list[index] * 2
+    my_list[index] = changed_index
+    return my_list
+
+#Uncomment the line below when your function is done
+print(double_index([3, 8, -10, 12], 3))
+
+# 5. Middle Item 
+
+# Create a function called middle_element that has one parameter named my_list.
+# If there are an odd number of elements in my_list, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+
+#Write your function here
+def middle_element(my_list):
+  if len(my_list) % 2 == 0:
+    middle1 = my_list[round(len(my_list) / 2 - 1) ]
+    middle2 = my_list[round(len(my_list) / 2)]
+    return (middle1 + middle2) / 2
+  else:
+    return my_list[round(len(my_list) / 2)]
+
+#Uncomment the line below when your function is done
+print(middle_element([5, 2, -10, -4, 4, 5]))
