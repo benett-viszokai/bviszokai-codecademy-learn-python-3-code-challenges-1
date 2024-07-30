68575f199f03c1ea2714068b427cda1616a4f22c
benett-viszokai <benettviszokai@gmail.com>
2024-07-30 10:15:39 +0200

    Update script.py

--- script.py
+++ script.py
@@ -365,3 +365,355 @@ def middle_element(my_list):
 
 #Uncomment the line below when your function is done
 print(middle_element([5, 2, -10, -4, 4, 5]))
+
+########################################################################################################################################################
+
+# V. Loops (Basic)
+
+# 1. Divisible By Ten
+
+# Create a function named divisible_by_ten() that takes a list of numbers named nums as a parameter.
+# Return the count of how many numbers in the list are divisible by 10.
+
+#Write your function here
+def divisible_by_ten(nums):
+  i = 0
+  for num in nums:
+    if num % 10 == 0:
+      i += 1
+  return i
+
+#Uncomment the line below when your function is done
+print(divisible_by_ten([20, 25, 30, 35, 40]))
+
+# 2. Greetings
+
+# Create a function named add_greetings() which takes a list of strings named names as a parameter.
+# In the function, create an empty list that will contain each greeting. Add the string 'Hello, ' in front of each name in names and append the greeting to the list.
+# Return the new list containing the greetings.
+
+#Write your function here
+def add_greetings(names):
+  greetings = []
+  for name in names:
+    greetings.append("Hello, " + name)
+  return greetings
+
+#Uncomment the line below when your function is done
+print(add_greetings(["Owen", "Max", "Sophie"]))
+
+# 3. Delete Starting Even Numbers
+
+# Write a function called delete_starting_evens() that has a parameter named my_list.
+# The function should remove elements from the front of my_list until the front of the list is not even. The function should then return my_list.
+# For example if my_list started as [4, 8, 10, 11, 12, 15], then delete_starting_evens(my_list) should return [11, 12, 15].
+# Make sure your function works even if every element in the list is even!
+
+def delete_starting_evens(my_list):
+  while (len(my_list) > 0 and my_list[0] % 2 == 0):
+    my_list = my_list[1:]
+  return my_list
+
+# 4. Odd Indices
+
+# Create a function named odd_indices() that has one parameter named my_list.
+# The function should create a new empty list and add every element from my_list that has an odd index. The function should then return this new list.
+# For example, odd_indices([4, 3, 7, 10, 11, -2]) should return the list [3, 10, -2].
+
+#Write your function here
+def odd_indices(my_list):
+  new_list = []
+  i = 0
+  while i < len(my_list):
+    if i % 2 != 0:
+      new_list.append(my_list[i])
+    i += 1
+  return new_list
+
+#Uncomment the line below when your function is done
+print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+# 5. Exponents
+
+# Create a function named exponents() that takes two lists as parameters named bases and powers. Return a new list containing every number in bases raised to every number in powers.
+
+#Write your function here
+def exponents(bases, powers):
+  new_list = []
+  for base in bases:
+    for power in powers:
+      new_list.append(base ** power)
+  return new_list
+
+#Uncomment the line below when your function is done
+print(exponents([2, 3, 4], [1, 2, 3]))
+
+########################################################################################################################################################
+
+# VI. Loops (Advanced)
+
+# 1. Larger Sum
+
+# Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+# The function should return the list whose elements sum to the greater number. If the sum of the elements of each list are equal, return lst1.
+
+#Write your function here
+def larger_sum(lst1, lst2):
+  sum1 = 0
+  sum2 = 0
+
+  for num in lst1:
+    sum1 += num
+  for num in lst2:
+    sum2 += num
+
+  if sum2 > sum1:
+    return lst2
+  else:
+    return lst1
+
+#Uncomment the line below when your function is done
+print(larger_sum([1, 9, 5], [2, 3, 7]))
+
+# 2. Over 9000
+
+# Create a function named over_nine_thousand() that takes a list of numbers named lst as a parameter.
+# The function should sum the elements of the list until the sum is greater than 9000. When this happens, the function should return the sum. If the sum of all of the elements is never greater than 9000, the function should return total sum of all the elements. If the list is empty, the function should return 0.
+# For example, if lst was [8000, 900, 120, 5000], then the function should return 9020.
+
+#Write your function here
+def over_nine_thousand(lst):
+  sum = 0
+  i = 0
+
+  while i < len(lst):
+    if sum < 9000:
+      sum += lst[i]
+      i += 1
+    else:
+      return sum
+  return sum
+    
+#Uncomment the line below when your function is done
+print(over_nine_thousand([8000, 900, 120, 5000]))
+
+# 3. Max Num
+
+# Create a function named max_num() that takes a list of numbers named nums as a parameter.
+# The function should return the largest number in nums
+
+#Write your function here
+def max_num(nums):
+  nums.sort()
+  return nums[-1]
+
+#Uncomment the line below when your function is done
+print(max_num([50, -10, 0, 75, 20]))
+
+# 4. Same Values
+
+# Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+# The function should return a list of the indices where the values were equal in lst1 and lst2.
+# For example, the following code should return [0, 2, 3]
+
+#Write your function here
+def same_values(lst1, lst2):
+  new_list = []
+  i = 0
+
+  while i < len(lst1):
+    if lst1[i] == lst2[i]:
+      new_list.append(i)
+    i += 1
+  return new_list
+
+
+#Uncomment the line below when your function is done
+print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5]))
+
+# 5. Reversed List
+
+# Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+# The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+# For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+
+#Write your function here
+def reversed_list(lst1, lst2):
+  lst2.sort() 
+  if lst1 == lst2:
+    return True
+  else:
+    return False
+
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+print(reversed_list([1, 5, 3], [3, 2, 1]))
+
+########################################################################################################################################################
+
+# VII. Functions (Basic)
+
+# 1. Tenth Power
+
+# Write a function named tenth_power() that has one parameter named num.
+# The function should return num raised to the 10th power.
+
+# Write your tenth_power function here:
+def tenth_power(num):
+  return num ** 10
+# Uncomment these function calls to test your tenth_power function:
+print(tenth_power(1))
+# 1 to the 10th power is 1
+print(tenth_power(0))
+# 0 to the 10th power is 0
+print(tenth_power(2))
+# 2 to the 10th power is 1024
+
+# 2. Square Root
+
+# Write a function named square_root() that has one parameter named num.
+# Use exponents (**) to return the square root of num.
+
+# Write your square_root function here:
+def square_root(num):
+  return num ** 0.5
+
+# Uncomment these function calls to test your square_root function:
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
+
+# 3. Win Percentage
+
+# Create a function called win_percentage() that takes two parameters named wins and losses.
+# This function should return out the total percentage of games won by a team based on these two numbers.
+
+# Write your win_percentage function here:
+def win_percentage(wins, losses):
+  return (wins + losses) / 100 * wins * 100
+# Uncomment these function calls to test your win_percentage function:
+print(win_percentage(5, 5))
+# should print 50
+print(win_percentage(10, 0))
+# should print 100
+
+# 4. Average
+
+# Write a function named average() that has two parameters named num1 and num2.
+# The function should return the average of these two numbers.
+
+# Write your average function here:
+def average(num1, num2):
+  return (num1 + num2) / 2
+# Uncomment these function calls to test your average function:
+print(average(1, 100))
+# The average of 1 and 100 is 50.5
+print(average(1, -1))
+# The average of 1 and -1 is 0
+
+# 5. Remainder
+
+# Write a function named remainder() that has two parameters named num1 and num2.
+# The function should return the remainder of twice num1 divided by half of num2.
+# Write your remainder function here:
+def remainder(num1, num2):
+  return (num1 * 2) % (num2 / 2)
+  
+# Uncomment these function calls to test your remainder function:
+print(remainder(15, 14))
+# should print 2
+print(remainder(9, 6))
+# should print 0
+
+########################################################################################################################################################
+
+# VIII. Functions (Advanced)
+
+# 1. First Three Multiples
+
+# Write a function named first_three_multiples() that has one parameter named num.
+# This function should print the first three multiples of num. Then, it should return the third multiple.
+# For example, first_three_multiples(7) should print 7, 14, and 21 on three different lines, and return 21.
+
+# Write your first_three_multiples function here
+def first_three_multiples(num):
+  i = 1
+  while i < 4:
+    print(num * i)
+    i += 1
+  return num * 3
+
+# Uncomment these function calls to test your first_three_multiples function:
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
+
+# 2. Tip
+
+# Create a function called tip() that has two parameters named total and percentage.
+# This function should return the amount you should tip given a total and the percentage you want to tip.
+
+# Write your tip function here:
+def tip(total, percentage):
+  return total * (percentage * 0.01)
+
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
+
+
+# 3. Bond, James Bond
+
+# Write a function named introduction() that has two parameters named first_name and last_name.
+# The function should return the last_name, followed by a comma, a space, first_name another space, and finally last_name.
+
+# Write your introduction function here:
+def introduction(first_name, last_name):
+  return last_name + ', ' + first_name + ' ' + last_name
+# Uncomment these function calls to test your introduction function:
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
+
+# 4. Dog Years
+# Some say that every one year of a human’s life is equivalent to seven years of a dog’s life. Write a function named dog_years() that has two parameters named name and age.
+# The function should compute the age in dog years and return the following string:
+"{name}, you are {age} years old in dog years"
+
+# Write your dog_years function here:
+def dog_years(name, age):
+  return name + ", you are " + str(age * 7) + " years old in dog years"
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+
+# 5. All Operations
+
+# Create a function named lots_of_math(). This function should have four parameters named a, b, c, and d. The function should print 3 lines and return 1 value.
+# First, print the sum of a and b.
+# Second, print c minus d.
+# Third, print the first number printed, multiplied by the second number printed.
+# Finally, return the third number printed modulo a.
+
+# Write your lots_of_math function here:
+def lots_of_math(a, b, c, d):
+  first = a + b
+  print(first)
+  second = c - d
+  print(second)
+  third = first * second
+  print(third)
+  return third % a
+
+# Uncomment these function calls to test your lots_of_math function:
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
